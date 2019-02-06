095c0742410acc1c2ec02c3e85104fadbd4cfe46
caiquecosta <ccosta22@live.com>
2019-02-06 17:05:00 -0500

    added chart-of-accounts and dashboard components

--- backend/project/users/views.py
+++ backend/project/users/views.py
@@ -1,12 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from .serializer import UserSerializer
-# from .models import Movie
-
-
-# class MovieViewSet(viewsets.ModelViewSet):
-#     queryset = Movie.objects.all()
-#     serializer_class = MovieSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
--- frontend/my-app/src/app/app.module.ts
+++ frontend/my-app/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ChartOfAccountsComponent } from './chart-of-accounts/chart-of-accounts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    DashboardComponent
+    DashboardComponent,
+    ChartOfAccountsComponent
   ],
   imports: [
     BrowserModule,
